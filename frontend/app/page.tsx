ac21d7c8a9803886232bbfcca2891d17ca38735f
infinitethoughts7 <rakeshganji007@gmail.com>
2025-12-27 21:36:33 +0530

    refactor: Move app directory from `src/app` to `app/` and update tsconfig path aliases.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home(){
   return ( 
     <div className="flex justify-center items-center h-screen bg-slate-200">
-      <h1 className="text-3xl text-slate-900 font-bold underline">Loan Home</h1>
+      <h1 className="text-3xl text-slate-900 font-bold underline">Loan and  Home</h1>
     </div>
   )
 }
